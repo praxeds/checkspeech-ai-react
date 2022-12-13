30c6d26cb683659d2c83304e3d838267a271992b
Juliana Praxedes <julianapraxeds@gmail.com>
2022-12-13 01:48:33 -0300

    Updated NavSection ScrollTrigger event with useEffect hook

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,24 @@
+import { useEffect } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-gsap.registerPlugin(ScrollTrigger);
 
-export default function Navbar() {  
-    const navbarAnim = gsap.from('.main-tool-bar', { 
-        yPercent: -100,
-        paused: true,
-        duration: 0.2
-      }).progress(1);
-      
-      ScrollTrigger.create({
-        start: "top top",
-        end: 99999,
-        onUpdate: (self) => {
-          self.direction === -1 ? navbarAnim.play() : navbarAnim.reverse()
-        }
-      });    
+export default function Navbar() { 
+    gsap.registerPlugin(ScrollTrigger);
+    useEffect(() => {
+        const navbarAnim = gsap.from('.main-tool-bar', { 
+            yPercent: -100,
+            paused: true,
+            duration: 0.2
+          }).progress(1);
+          ScrollTrigger.create({
+            start: "top top",
+            end: 99999,
+            onUpdate: (self) => {
+              self.direction === -1 ? navbarAnim.play() : navbarAnim.reverse()
+            }
+          }); 
+    }, []);
+         
     return (
         <div className="Navbar">
             <div className='main-tool-bar'>Test</div>
